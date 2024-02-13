9c5a08d42c660e5f9025348f57c4b67221248f58
corex <aulia.ikbal09@gmail.com>
2024-02-13 11:21:14 +0700

    first init resto backend

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    // index
+    public function index(Request $request)
+    {
+        // get all users with pagination
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                $query->where('name', 'like', '%' . $name . '%')
+                    ->orwhere('name', 'like', '%' . $name . '%');
+            })
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+
+    // create
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    // store
+    public function store(Request $request)
+    {
+        // validate the request...
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'role' => 'required|in:admin,staff,user',
+        ]);
+
+        // store the request
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->role = $request->role;
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User created successfully');
+
+    }
+
+    // show
+    public function show($id)
+    {
+        return view('pages.users.show');
+    }
+
+    // edit
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('pages.users.edit', compact('user'));
+    }
+
+    // update
+    public function update(Request $request, $id)
+    {
+        // validate the request...
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'role' => 'required|in:admin,staff,user',
+        ]);
+
+        // store the request
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        // $user->password = Hash::make($request->password);
+        $user->role = $request->role;
+
+        // if password is not empty
+        if ($request->password) {
+            $user->password = Hash::make($request->password);
+            // echo "ada password\n";
+        }
+        // print_r($user);
+        $user->save();
+
+        return redirect()->route('users.index')->with('success', 'User update successfully');
+    }
+
+    //destroy
+    public function destroy($id)
+    {
+        // delete the request
+        $user = User::find($id);
+        $user->delete();
+
+        return redirect()->route('users.index')->with('success', 'User deleted successfully');
+    }
+}
